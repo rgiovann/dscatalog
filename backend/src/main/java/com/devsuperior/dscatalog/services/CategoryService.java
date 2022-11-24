7f23ce6920ff5ab9ff7348a365febffa32833c52
Giovani L. Rozza <giovanni.rozza@gmail.com>
2022-11-24 17:40:13 -0300

    H2 Database seeding.

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -17,6 +18,8 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	// ACID properties
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 		
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Electronics');
+INSERT INTO tb_category (name) VALUES ('Computer');
+INSERT INTO tb_category (name) VALUES ('Music');
+INSERT INTO tb_category (name) VALUES ('Car&Audio');
