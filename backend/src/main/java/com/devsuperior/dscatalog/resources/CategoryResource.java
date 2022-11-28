4c0be31ca63ec7c5b93450e4e5cb2e36b3c207f1
Giovani L. Rozza <giovanni.rozza@gmail.com>
2022-11-28 18:08:42 -0300

    Added delete REST API.

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,7 +56,12 @@ public class CategoryResource {
 		
 	}
 	
-	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
 	
  
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -6,12 +6,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 // this annotation register this class as a component 
@@ -32,31 +35,39 @@ public class CategoryService {
 	}
 
 	@Transactional(readOnly = true)
-	public CategoryDTO findById(Long id) {		
-		return new CategoryDTO (repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Cannot find requested category.")));
+	public CategoryDTO findById(Long id) {
+		return new CategoryDTO(repository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("Cannot find requested category.")));
 	}
 
 	@Transactional
 	public CategoryDTO insert(CategoryDTO categoryDTO) {
 		Category entity = new Category();
 		entity.setName(categoryDTO.getName());
-		entity = repository.save(entity);       // reposity.save() returns a reference to object saved in DB
+		entity = repository.save(entity); // reposity.save() returns a reference to object saved in DB
 		return new CategoryDTO(entity);
 	}
-	
+
 	@Transactional
 	public CategoryDTO update(Long id, CategoryDTO categoryDTO) {
 		try {
-		Category proxyEntity = repository.getReferenceById(id);
-		proxyEntity.setName(categoryDTO.getName());
-		proxyEntity = repository.save(proxyEntity);
-		return new CategoryDTO(proxyEntity);
-		}
-		catch (EntityNotFoundException e)
-		{
+			Category proxyEntity = repository.getReferenceById(id);
+			proxyEntity.setName(categoryDTO.getName());
+			proxyEntity = repository.save(proxyEntity);
+			return new CategoryDTO(proxyEntity);
+		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Error. Id not found : " + id);
 		}
 	}
-	
 
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Error. Id not found: " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Error. Integrity violation: " + id);
+		}
+
+	}
 }
